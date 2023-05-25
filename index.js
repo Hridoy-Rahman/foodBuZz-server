885d586595dc9b07959e01c033dafcd567a8242d
Hridoy-Rahman <hridoyrahman715@gmail.com>
2023-05-25 16:46:31 +0600

    checking the server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express =require ('express')
+const cors=require('cors')
+
+const app=express()
+
+const allChef=require('./chef.json')
+
+const port=process.env.PORT||5000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('Server is running')
+})
+
+app.listen(port,()=>{
+    console.log(`FoodBuZZ server is running on port ${port}`)
+})

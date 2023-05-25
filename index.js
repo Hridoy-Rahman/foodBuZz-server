066b13489095c7f3702c0258dcc12b665ea6852c
Hridoy-Rahman <hridoyrahman715@gmail.com>
2023-05-25 16:53:23 +0600

    single data

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const express =require ('express')
-const cors=require('cors')
+const express = require('express')
+const cors = require('cors')
 
-const app=express()
+const app = express()
 
-const allChef=require('./chef.json')
+const allChef = require('./chef.json')
 
-const port=process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Server is running')
 })
-app.get('/allChef',(req,res)=>{
+app.get('/allChef', (req, res) => {
     res.send(allChef)
 })
 
-app.listen(port,()=>{
+app.get('/allChef/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = allChef.find((chef) => chef.id === id);
+    res.send(chef);
+});
+
+app.listen(port, () => {
     console.log(`FoodBuZZ server is running on port ${port}`)
 })
